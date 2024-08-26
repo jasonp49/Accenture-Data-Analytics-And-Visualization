4d1037a916cd71a49cd24e5bd69891d57aa6d6d9
Jason Pauley <jasonpauley@gmail.com>
2024-08-26 16:05:19 -0700

    Update SocialBuzzDataAnalysis.sql

--- SocialBuzzDataAnalysis.sql
+++ SocialBuzzDataAnalysis.sql
@@ -2,11 +2,11 @@
 SELECT DISTINCT(Category)
 FROM Content
 
---remove " from beginning and end of category naames, make all lowercase
+--remove " from beginning and end of category names, make all lowercase
 SELECT DISTINCT(LOWER(TRIM(Category, '"')))
 FROM Content
 
---find columns qith missing data
+--find columns with missing data
 SELECT *
 FROM Reactions
 WHERE TYPE IS NULL
@@ -17,10 +17,11 @@ FROM Reactions
 WHERE TYPE IS NULL
 
 --find total posts, total points and average score for each category
-SELECT DISTINCT(LOWER(TRIM(Category, '"'))) AS clean_category,
-COUNT(r.Reaction_ID)                        AS total_posts,
-SUM(t.Score)                                AS total_score,
-SUM(t.Score) / COUNT(r.Reaction_ID)         AS avg_Score
+SELECT 
+	DISTINCT(LOWER(TRIM(Category, '"'))) AS clean_category,
+	COUNT(r.Reaction_ID)                 AS total_posts,
+	SUM(t.Score)                         AS total_score,
+	SUM(t.Score) / COUNT(r.Reaction_ID)  AS avg_Score
 FROM Content    AS c
 JOIN Reactions  AS r
 ON r.Content_ID  = c.Content_ID
@@ -29,7 +30,7 @@ ON r.Type = t.Type
 GROUP BY 1
 ORDER BY 2 DESC;
 
---find totla reactions per month
+--find total reactions per month
 SELECT 
 	DISTINCT strftime('%m', Datetime) AS month,
 	COUNT(Reaction_ID)                AS total_reactions
@@ -38,9 +39,10 @@ GROUP BY 1
 ORDER BY 1
 
 --DEC and JAN had higher than avg reactions.  Find which categories excelled here
-SELECT DISTINCT(LOWER(TRIM(Category, '"'))) AS clean_category,
-COUNT(r.Reaction_ID)                        AS DEC_JAN_posts,
-SUM(t.Score)                                AS total_score
+SELECT 
+	DISTINCT(LOWER(TRIM(Category, '"'))) AS clean_category,
+	COUNT(r.Reaction_ID)                 AS DEC_JAN_posts,
+	SUM(t.Score)                         AS total_score
 FROM Content    AS c
 JOIN Reactions  AS r
 ON r.Content_ID  = c.Content_ID
